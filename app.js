7a574356dc4b3666ac3426c1e3631d246128c908
Geoff Guidetti <geoffguidetti@gmail.com>
2017-01-29 15:38:06 -0800

    Installs working server on port 3000

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var vhost = require( 'vhost' );
+
+function createVirtualHost(domainName, dirPath) {
+    return vhost(domainName, express.static( dirPath ));
+}
+
+var stoopitHost = createVirtualHost("kaleidoscope.wtf", "kaleidoscope.wtf");
+var alefbetHost = createVirtualHost("alefbetquiz.com", "alefbetquiz.com");
+
+//Use the virtual hosts
+app.use(stoopitHost);
+app.use(alefbetHost);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
