3337068d87daa45e610e1472d937760288b0a04f
Geoff Guidetti <geoffguidetti@gmail.com>
2017-01-29 17:42:09 -0800

    configure for https

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var application_root = __dirname;
 var express = require('express');
 var vhost = require( 'vhost' );
+var https = require('https');
+var http = require('http');
 
 var app = express();
+var credentials = {};
 
 var config = require('./config.json')[process.env.NODE_ENV || 'dev'];
 
@@ -21,6 +24,17 @@ app.get('/', function (req, res) {
   res.send('Hello ya jerk!')
 })
 
-app.listen(config.port, function () {
-  console.log('Example app listening on port '+config.port+'!')
-})
+var httpServer = http.createServer(app);
+if(config.name == "prod"){
+	var options = {
+	     key: fs.readFileSync('/etc/letsencrypt/live/kaleidoscope.wtf/privkey.pem'),
+	     cert: fs.readFileSync('/etc/letsencrypt/live/kaleidoscope.wtf/fullchain.pem'),
+	     ca: fs.readFileSync('/etc/letsencrypt/live/kaleidoscope.wtf/chain.pem')
+	}
+
+	var httpsServer = https.createServer(options, app);
+	httpsServer.listen(443);
+}
+
+console.log('['+config.name+'] starting on port',config.port);
+httpServer.listen(config.port);
