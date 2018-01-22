a4d13b76fee97dc64896dd902a0969e3bdfa42b2
Geoff Guidetti <geoffguidetti@gmail.com>
2018-01-22 12:27:14 -0800

    Make error message nicer.

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(vhost('geoguide.me',express.static(__dirname + '/geoguide.me')));
 
 
 app.get('/', function (req, res) {
-  res.send('Hello ya jerk!')
+  res.send('vhosts didn\'t catch this! :(')
 });
 
 var httpServer = http.createServer(app);
