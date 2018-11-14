856f00c9b6a9d0e30b80353bc405d923ba953cb5
Geoff Guidetti <geoffguidetti@gmail.com>
2018-11-13 18:57:51 -0800

    Do something asshole

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.use(vhost('*.alefbetquiz.com', express.static(path.join(__dirname, '/alefbet
 app.use(vhost('kaleidoscope.wtf',express.static(__dirname + '/kaleidoscope.wtf')));
 app.use(vhost('geoguide.me',express.static(__dirname + '/geoguide.me')));
 app.use(vhost('*.snackbrigade.com',express.static(__dirname + '/snackbrigade.com')));
-app.use(vhost('*.scuar.agency',express.static(__dirname + '/scuar.agency', {
-  extensions: ['html', 'htm','ejs'],
+app.use(vhost('*.scuar.agency',express.static(__dirname + '/scuar', {
+  extensions: ['html', 'htm','ejs']
 })));
 
 
